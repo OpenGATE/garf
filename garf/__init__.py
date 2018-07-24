a1a8f7a39e89970f528e52c2650eb0ab4526d234
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2018-07-24 16:36:48 +0200

    initial commit

---
+++ garf/__init__.py
@@ -0,0 +1,3 @@
+
+# import files
+from .helpers import *
---
+++ garf/helpers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+
+def my_hello_world():
+    '''
+    Test
+    '''
+    print('My Hello world')
+
+
+
+def my_hello_world2():
+    '''
+    Test
+    '''
+    print('TER My Hello world OKOKOOKK')
+
