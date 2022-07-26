3887cd709885afaa75a7c4407e5e1251a2acf82c
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2022-07-26 18:07:41 +0200

    update uproot

--- garf/helpers.py
+++ garf/helpers.py
@@ -4,6 +4,7 @@ import os
 import uproot
 import copy
 
+
 # -----------------------------------------------------------------------------
 def load_training_dataset(filename):
     '''
@@ -11,14 +12,14 @@ def load_training_dataset(filename):
     '''
     # Check if file exist
     if (not os.path.isfile(filename)):
-        print("File '"+filename+"' does not exist.")
+        print("File '" + filename + "' does not exist.")
         exit()
 
     # Check if this is a root file
     try:
         f = uproot.open(filename)
     except Exception:
-        print("File '"+filename+"' cannot be opened, not a root file ?")
+        print("File '" + filename + "' cannot be opened, not a root file ?")
         exit()
 
     # Look for a single key named "ARF (training)"
@@ -30,14 +31,14 @@ def load_training_dataset(filename):
         exit()
 
     # Convert to arrays
-    a = data.arrays()
-    theta = a[b'Theta']
-    phi = a[b'Phi']
-    E = a[b'E']
-    if (b'w' in data.keys()):
-        w = a[b'w']
+    a = data.arrays(library='np')
+    theta = a['Theta']
+    phi = a['Phi']
+    E = a['E']
+    if ('w' in data.keys()):
+        w = a['w']
     else:
-        w = a[b'window']
+        w = a['window']
     data = np.column_stack((theta, phi, E, w))
 
     return data, theta, phi, E, w
@@ -48,21 +49,25 @@ def print_training_dataset_info(data, rr=40):
     '''
     Print training dataset information
     '''
-    theta = data[:,0]
-    phi = data[:,1]
-    E = data[:,2]
-    w = data[:,3]
+    theta = data[:, 0]
+    phi = data[:, 1]
+    E = data[:, 2]
+    w = data[:, 3]
 
     n = len(w)
-    n_nd = len(w[w==0])
+    # outside window is zero
+    n_nd = len(w[w == 0])
     print('Nb of particles:              {}'.format(n))
-    print(f'Nb of particles (RR):         {n_nd*rr+n-n_nd}')
-    print('Nb of non-detected particles: {} {:.2f}%'.format(n_nd, n_nd/n*100))
-    print('Nb of detected particles:     {} {:.2f}%'.format(n-n_nd, (n-n_nd)/n*100))
+    for i in range(6):
+        print(f'Nb of particle in windows {i} : {len(w[w == i])}')
+    print(f'RR for outside particle:     {rr}')
+    print(f'Nb of particles (RR):         {n_nd * rr + n - n_nd}')
+    print('Nb of non-detected particles: {} {:.2f}%'.format(n_nd, n_nd / n * 100))
+    print('Nb of detected particles:     {} {:.2f}%'.format(n - n_nd, (n - n_nd) / n * 100))
 
     print('Min Max theta {:.2f} {:.2f} deg'.format(np.amin(theta), np.amax(theta)))
     print('Min Max phi   {:.2f} {:.2f} deg'.format(np.amin(phi), np.amax(phi)))
-    print('Min Max E     {:.2f} {:.2f} keV'.format(np.amin(E*1000), np.amax(E*1000)))
+    print('Min Max E     {:.2f} {:.2f} keV'.format(np.amin(E * 1000), np.amax(E * 1000)))
 
 
 # -----------------------------------------------------------------------------
@@ -73,14 +78,14 @@ def load_test_dataset(filename):
 
     # Check if file exist
     if (not os.path.isfile(filename)):
-        print("File '"+filename+"' does not exist.")
+        print("File '" + filename + "' does not exist.")
         exit()
 
     # Check if this is a root file
     try:
         f = uproot.open(filename)
     except Exception:
-        print("File '"+filename+"' cannot be opened, not a root file ?")
+        print("File '" + filename + "' cannot be opened, not a root file ?")
         exit()
 
     # Look for a single key named "ARF (training)"
@@ -93,14 +98,14 @@ def load_test_dataset(filename):
 
     # Convert to arrays
     a = data.arrays()
-    theta = a[b'Theta']
-    phi = a[b'Phi']
-    E = a[b'E']
-    x = a[b'X']
-    y = a[b'Y']
+    theta = a['Theta']
+    phi = a['Phi']
+    E = a['E']
+    x = a['X']
+    y = a['Y']
     debug = True
-    if debug: # FIXME DEBUG 
-        evid = a[b'eventID']
+    if debug:  # FIXME DEBUG
+        evid = a['eventID']
         data = np.column_stack((x, y, theta, phi, E, evid))
         print('DEBUG eventID')
     else:
@@ -127,27 +132,27 @@ def image_uncertainty_arf(data, sq_data, N, threshold):
 
         # Chetty2007 p 4832 history by history
         a = np.divide(sq_d, N)
-        b = np.square(np.divide(d,N))
-        m = (a-b)
-        m = m/(N-1)
+        b = np.square(np.divide(d, N))
+        m = (a - b)
+        m = m / (N - 1)
         sm = np.sqrt(m)
 
         # number of counts (per N)
         dn = np.divide(d, N)
 
         # normalisation by the nb of counts dn
-        t = np.max(d)*threshold
+        t = np.max(d) * threshold
         sigma = np.divide(sm, dn, out=np.zeros_like(dn), where=d > t)
 
         uncert[slice_i] = sigma
 
         # compute the mean over all pixels with more than threshold counts
-        #n = np.where(d > threshold)
+        # n = np.where(d > threshold)
         n = np.where(d > t)
         n = len(n[0])
         sum_sigma = np.sum(sigma)
         if sum_sigma:
-            mean_sigma = sum_sigma/n
+            mean_sigma = sum_sigma / n
         else:
             mean_sigma = 1
 
@@ -174,14 +179,14 @@ def image_uncertainty_analog(data, threshold):
         d = data[slice_i]
         # sqrt of the variance (var is equal to the value itself)
         sigma = np.sqrt(d)
-        t = np.max(d)*threshold
+        t = np.max(d) * threshold
         sigma_n = np.divide(sigma, d, out=np.zeros_like(d), where=d > t)
         uncert[slice_i] = sigma_n
         sum_sigma = np.sum(sigma_n)
-        #n_sigma = np.where(d > threshold) ## BUG !!
+        # n_sigma = np.where(d > threshold) ## BUG !!
         n_sigma = np.where(d > t)
         n_sigma = len(n_sigma[0])
-        mean_sigma = sum_sigma/n_sigma
+        mean_sigma = sum_sigma / n_sigma
         # end
         s_sigma[slice_i] = mean_sigma
         slice_i = slice_i + 1
